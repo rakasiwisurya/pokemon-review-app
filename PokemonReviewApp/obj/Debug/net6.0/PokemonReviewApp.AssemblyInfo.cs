a51bbdd38327819f167b8da887cb7738f0eef247
Rakasiwi Surya <rakasiwi.surya@gmail.com>
2025-07-30 18:19:09 +0700

    done repository pattern and dependency injection setup

--- PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
+++ PokemonReviewApp/obj/Debug/net6.0/PokemonReviewApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a39032751bde9dd8995c9a2c252ea49ea40d37c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b46ac3d4c43a75ae832fc5943cb8e0594bddb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonReviewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
